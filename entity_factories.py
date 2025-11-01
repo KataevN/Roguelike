f89e30f48e22f9033234fbb64a9479fb29053e58
Nikolai Kataev <nik2675819@yandex.ru>
2025-11-01 17:14:09 +0100

    enemy following system

--- entity_factories.py
+++ entity_factories.py
@@ -1,6 +1,27 @@
-from entity import Entity
+from components.ai import HostileEnemy
+from components.fighter import Fighter
+from entity import Actor
 
-player = Entity(char="@", color=(255, 255, 255), name="Player", blocks_movement=True)
+player = Actor(
+    char="@",
+    color=(255, 255, 255), 
+    name="Player", 
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=30, defense=2, power=5),
+    )
 
-rat = Entity(char="r", color=(63, 127, 63), name="Rat", blocks_movement=True)
-zombie = Entity(char="Z", color=(0, 127, 0), name="Zombie", blocks_movement=True)
+rat = Actor(
+    char="r", 
+    color=(63, 127, 63), 
+    name="Rat", 
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=10, defense=0, power=5),
+    )
+
+zombie = Actor(
+    char="Z", 
+    color=(0, 127, 0), 
+    name="Zombie", 
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=16, defense=1, power=4),
+    )
